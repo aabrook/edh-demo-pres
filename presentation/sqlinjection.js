92f4f8ad88f1ec992387f8bdb502472b9fa25311
aabrook <aabrook@gmail.com>
2016-11-16 21:37:16 +1000

    Start presentation prep

---
+++ presentation/sqlinjection.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import {
+  Appear,
+  BlockQuote,
+  Cite,
+  CodePane,
+  Deck,
+  Fill,
+  Heading,
+  Image,
+  Layout,
+  Link,
+  ListItem,
+  List,
+  Markdown,
+  Quote,
+  Slide,
+  Spectacle,
+  Text
+} from "spectacle";
+
+export default () => (
+  <Slide>
+    <Header>Boom</Header>
+  </Slide>
+)
